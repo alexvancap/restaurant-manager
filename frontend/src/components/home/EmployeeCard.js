149846b4ea651ec2a203a8cf70b700d5623726d7
David Jaworski <jaws360@gmail.com>
2020-01-23 14:46:03 -0600

    added employee fire and hire functionality

--- frontend/src/components/home/EmployeeCard.js
+++ frontend/src/components/home/EmployeeCard.js
@@ -1,32 +1,40 @@
-import React from 'react'
+import React from "react";
 
-export default class EmployeeCard extends React.Component{
-
-    render(){
-        return(
-            <div className="ui card">
-                <div className="image">
-                    <img src="/images/avatar2/large/kristy.png" alt=""></img>
-                </div>
-                <div className="content">
-                    <a className="header" href="/#">{this.props.employee.name}</a>
-                    <div className="meta">
-                    <span className="date">Joined in 2013</span>
-                    </div>
-                    <div className="description">
-                        {
-                            `phone: ${this.props.employee.phone} \n
-                            email: ${this.props.employee.email} \n
-                        `}
-                    </div>
-                </div>
-                <div className="extra content">
-                    <a href="/#">
-                        <i className="user icon"></i>
-                        22 Friends
-                    </a>
-                </div>
-            </div>
-        )
-    }
+export default class EmployeeCard extends React.Component {
+  handleEmployee = () => {
+    fetch(`http://localhost:3000/delete_employee/${this.props.employee.id}`, {
+      method: "DELETE"
+    });
+  };
+  render() {
+    return (
+      <div className="ui card">
+        <div className="image">
+          <img src="/images/avatar2/large/kristy.png" alt=""></img>
+        </div>
+        <div className="content">
+          <a className="header" href="/#">
+            {this.props.employee.name}
+          </a>
+          <div className="meta">
+            <span className="date">Joined in 2013</span>
+          </div>
+          <div className="description">
+            {`phone: ${this.props.employee.phone} \n
+              email: ${this.props.employee.email} \n
+            `}
+          </div>
+          <form onClick={this.handleEmployee}>
+            <button>Fire!</button>
+          </form>
+        </div>
+        <div className="extra content">
+          <a href="/#">
+            <i className="user icon"></i>
+            22 Friends
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
--- frontend/src/components/home/EmployeesContainer.js
+++ frontend/src/components/home/EmployeesContainer.js
@@ -1,37 +1,102 @@
-import React from 'react'
-import EmployeeCard from './EmployeeCard'
+import React from "react";
+import EmployeeCard from "./EmployeeCard";
+import HireEmployeeCard from "./HireEmployeeCard";
 
 export default class HomeContainer extends React.Component {
-    state = {
-        user: {
-            restaurants: [
+  state = {
+    user: {
+      restaurants: []
+    },
+    employeeName: "",
+    employeeEmail: "",
+    employeePhone: "",
+    employeeRole: "",
+    rendered: false
+  };
 
-            ]
-        },
-        rendered: false
-    }
-
-    componentDidMount() {
-        if (localStorage.token) {
-            fetch("http://localhost:3000/get_user_by_token", {
-                headers: {
-                    Authorization: `Bearer ${localStorage.token}`
-                }
-            }).then(res => res.json())
-                .then(res => {
-                    this.setState({ user: res, rendered: true })
-                })
+  componentDidMount() {
+    if (localStorage.token) {
+      fetch("http://localhost:3000/get_user_by_token", {
+        headers: {
+          Authorization: `Bearer ${localStorage.token}`
         }
+      })
+        .then(res => res.json())
+        .then(res => {
+          this.setState({ user: res, rendered: true });
+        });
     }
-    render() {
-        return (
-            <div id="mainDiv" style={{maxWidth: "960px", margin: "0 auto"}}>
-                {this.state.user.restaurants.map(restaurant => {
-                    return restaurant.employees.map(employee =>
-                        <EmployeeCard key={employee.id} employee={employee} restaurant={restaurant} />
-                    )
-                })}
-            </div>
-        )
-    }
+  }
+
+  setEmployeeName = event => {
+    this.setState({
+      employeeName: event
+    });
+  };
+
+  setEmployeeEmail = event => {
+    this.setState({
+      employeeEmail: event
+    });
+  };
+
+  setEmployeePhone = event => {
+    this.setState({
+      employeePhone: event
+    });
+  };
+
+  setEmployeeRole = event => {
+    this.setState({
+      employeeRole: event
+    });
+  };
+
+  handleSubmit = () => {
+    fetch("http://localhost:3000/add_employee", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: this.state.employeeName,
+        email: this.state.employeeEmail,
+        phone: this.state.employeePhone,
+        role: this.state.employeeRole
+      })
+    })
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          user: {
+            ...this.state.user,
+            restaurants: [...this.state.user.restaurants, { employees: [res] }]
+          }
+          //   user: [...this.state.user.restaurants.employees, res]
+        })
+      );
+  };
+
+  render() {
+    return (
+      <div id="mainDiv" style={{ maxWidth: "960px", margin: "0 auto" }}>
+        <HireEmployeeCard
+          setEmployeeName={this.setEmployeeName}
+          setEmployeeEmail={this.setEmployeeEmail}
+          setEmployeePhone={this.setEmployeePhone}
+          setEmployeeRole={this.setEmployeeRole}
+          onSubmit={this.handleSubmit}
+        />
+        {this.state.user.restaurants.map(restaurant => {
+          return restaurant.employees.map(employee => (
+            <EmployeeCard
+              key={employee.id}
+              employee={employee}
+              restaurant={restaurant}
+            />
+          ));
+        })}
+      </div>
+    );
+  }
 }
